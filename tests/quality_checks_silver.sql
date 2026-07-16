632385ffeadb0c17a0abc26e13ff794a5fee774a
Sedki Chalghoumi <130706213+SedkiC@users.noreply.github.com>
2026-07-16 14:45:13 -0400

    Update and rename placeholder to quality_checks_silver.sql

---
+++ tests/quality_checks_silver.sql
@@ -0,0 +1,175 @@
+/*
+===============================================================================
+Quality Checks
+===============================================================================
+Script Purpose:
+    This script performs various quality checks for data consistency, accuracy, 
+    and standardization across the 'silver' layer. It includes checks for:
+    - Null or duplicate primary keys.
+    - Unwanted spaces in string fields.
+    - Data standardization and consistency.
+    - Invalid date ranges and orders.
+    - Data consistency between related fields.
+
+Usage Notes:
+    - Run these checks after data loading Silver Layer.
+    - Investigate and resolve any discrepancies found during the checks.
+===============================================================================
+*/
+
+--crm_cust_info Table
+--Check for Nulls or Duplicates in Primary Key
+--Expectations: No Nulls and No Duplicates
+SELECT cst_id, COUNT(*)
+FROM bronze.crm_cust_info
+GROUP BY cst_id
+HAVING COUNT(*) > 1 OR cst_id IS NULL
+
+--Check for unwanted spaces (check for all string columns)
+--Expectations: No results
+SELECT cst_firstname
+FROM bronze.crm_cust_info
+WHERE cst_firstname != TRIM(cst_firstname)
+
+--Check for Data Standardization & Consistency Issues for low Cardinality Columns 
+SELECT DISTINCT cst_gndr
+FROM bronze.crm_cust_info
+
+--crm_prd_info Table
+--Check for unwanted spaces (check for all string columns)
+--Expectations: No results
+SELECT prd_nm
+FROM bronze.crm_prd_info
+WHERE prd_nm != TRIM(prd_nm)
+
+--Check for NULLS or Negative Numbers
+SELECT prd_cost
+FROM bronze.crm_prd_info
+WHERE prd_cost < 0 OR prd_cost IS NULL
+
+--Check for Data Standardization & Consistency Issues for low Cardinality Columns 
+SELECT DISTINCT prd_line
+FROM bronze.crm_prd_info
+
+--Check for Invalid Date Orders
+SELECT * 
+FROM bronze.crm_prd_info
+WHERE prd_end_dt < prd_start_dt
+
+--Check Quality silver.crm_prd_info Table
+SELECT prd_id, COUNT(*)
+FROM silver.crm_prd_info
+GROUP BY prd_id
+HAVING COUNT(*) > 1 OR prd_id IS NULL
+
+SELECT prd_cost
+FROM silver.crm_prd_info
+WHERE prd_cost < 0 OR prd_cost IS NULL
+
+SELECT DISTINCT prd_line
+FROM silver.crm_prd_info
+
+SELECT * 
+FROM silver.crm_prd_info
+WHERE prd_end_dt < prd_start_dt
+
+SELECT * 
+FROM silver.crm_prd_info
+
+--crm_sales_details Table
+--Check for unwanted spaces
+SELECT
+	sls_ord_num
+FROM bronze.crm_sales_details
+WHERE sls_ord_num != TRIM(sls_ord_num)
+
+--Check for integrity of Key Columns
+SELECT *
+FROM bronze.crm_sales_details
+WHERE sls_prd_key NOT IN (SELECT prd_key FROM silver.crm_prd_info)
+
+SELECT *
+FROM bronze.crm_sales_details
+WHERE sls_cust_id NOT IN (SELECT cst_id FROM silver.crm_cust_info)
+
+--Check for Invalid Dates
+SELECT
+    sls_order_dt 
+FROM bronze.crm_sales_details
+WHERE sls_order_dt <= 0 
+OR LEN(sls_order_dt) != 8 
+OR sls_order_dt > 20500101 
+OR sls_order_dt < 19000101
+
+SELECT *
+FROM bronze.crm_sales_details
+WHERE sls_order_dt > sls_ship_dt OR sls_order_dt > sls_due_dt
+
+-- Sales = Quantity * Price (No Negative, Zeros Or Nulls) -- Check for Business Logic to handle data quatlity issues
+-- If Sales Null, Negative or Zero ==> Sales = Quantity * Price
+-- If Price is Wrong ==> Price = Sales / Quantity
+-- If Price is Negative, Convert to positive
+SELECT DISTINCT
+    sls_sales,
+    sls_quantity,
+    sls_price
+FROM bronze.crm_sales_details
+WHERE sls_sales != sls_quantity * sls_price 
+OR sls_sales IS NULL OR sls_quantity IS NULL OR sls_price IS NULL
+OR sls_sales <= 0 OR sls_quantity <= 0 OR sls_price <= 0
+ORDER BY sls_sales, sls_quantity, sls_price
+
+SELECT *
+FROM silver.crm_sales_details
+
+SELECT DISTINCT
+    sls_sales,
+    sls_quantity,
+    sls_price
+FROM silver.crm_sales_details 
+WHERE sls_sales != sls_quantity * sls_price 
+OR sls_sales IS NULL OR sls_quantity IS NULL OR sls_price IS NULL
+OR sls_sales <= 0 OR sls_quantity <= 0 OR sls_price <= 0
+ORDER BY sls_sales, sls_quantity, sls_price
+
+--erp_cust_az12
+--Check Columns 
+SELECT 
+	 cid,  
+	 bdate,
+	 gen 
+FROM bronze.erp_cust_az12
+
+--Check Dates
+SELECT 
+	 bdate
+FROM bronze.erp_cust_az12
+WHERE bdate < '1924-01-01' OR bdate > GETDATE()
+
+--Check Data Standardization & Consistency
+SELECT DISTINCT gen,
+FROM bronze.erp_cust_az12
+
+SELECT *
+FROM silver.erp_cust_az12
+
+--erp_loc_a101
+--Check Columns and Cardinality
+
+SELECT DISTINCT cntry FROM bronze.erp_loc_a101
+
+SELECT * FROM silver.erp_loc_a101
+
+SELECT DISTINCT cntry FROM silver.erp_loc_a101
+
+--erp_px_cat_g1v2
+--Check for unwanted spaces
+ SELECT * FROM bronze.erp_px_cat_g1v2
+ WHERE cat != TRIM(cat) OR subcat != TRIM(subcat) OR maintenance != TRIM(maintenance) 
+
+ --Cardinality
+ SELECT DISTINCT
+ cat, subcat, maintenance
+ FROM bronze.erp_px_cat_g1v2
+
+ SELECT * FROM silver.erp_px_cat_g1v2
